ee1a501244a6498971de9d4ed3acd15dee0e617b
manikats <vjmaniac@live.com>
2025-02-26 01:34:35 +0100

    Update python3 bot.py

--- python3 bot.py
+++ python3 bot.py
@@ -195,3 +195,5 @@ if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    asyncio.run(main())
